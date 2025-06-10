7865b8a04e41977c57ff03411bf1fc7500e44559
Agieahmadd <agieaeae@gmail.com>
2025-06-10 13:09:26 +0800

    initial commit

---
+++ DataDiri.java
@@ -0,0 +1,13 @@
+public class DataDiri {
+
+    private String nama;
+    private String npm;
+    private String kelas;
+
+    
+    public DataDiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+} 
